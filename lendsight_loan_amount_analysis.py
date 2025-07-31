186c66e1c6cbbfbafc11dfc7ee720c589261b76f
Holymichael99 <omofolarin1994@gmail.com>
2025-07-31 13:00:01 +0100

    Create Total Income column

--- lendsight_loan_amount_analysis.py
+++ lendsight_loan_amount_analysis.py
@@ -11,3 +11,15 @@ df['Gender'].fillna('Unknown', inplace=True)
 df['Self_Employed'].fillna('Unknown', inplace=True)
 df['Dependents'] = df['Dependents'].replace('3+', 3).fillna('0')
 df['Dependents'] = df['Dependents'].astype(str)
+
+# Create TotalIncome column
+df['TotalIncome'] = df['ApplicantIncome'] + df['CoapplicantIncome']
+
+# --- Plot 1: Loan Amount vs Total Income ---
+plt.figure(figsize=(8, 5))
+sns.scatterplot(data=df, x='TotalIncome', y='LoanAmount')
+plt.title('Loan Amount vs Total Income')
+plt.xlabel('Total Income')
+plt.ylabel('Loan Amount')
+plt.tight_layout()
+plt.show()
